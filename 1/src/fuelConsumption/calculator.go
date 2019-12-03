76644a0adc8904c76a6ab50fcaaad5965b61e320
Danny Boyer <danny.boyer@bnc.ca>
2019-12-03 08:54:45 -0500

    Removed testInput, Removed binary, Used short var assignment

--- 1/src/fuelConsumption/calculator.go
+++ 1/src/fuelConsumption/calculator.go
@@ -1,43 +1,40 @@
 package main
 
 import (
-	"fmt" 
-	"math"
 	"bufio"
-	"os"
+	"fmt"
 	"log"
+	"math"
+	"os"
 	"strconv"
 )
 
 func compute(mass float64) float64 {
-	return math.Floor(mass / 3) - 2
+	return math.Floor(mass/3) - 2
 }
 
 func computeRec(mass float64) float64 {
-	var fuel = math.Floor(mass / 3) - 2
+	fuel := math.Floor(mass/3) - 2
 	if fuel <= 0 {
-		return 0	
-	} 
-	return fuel + computeRec(fuel) 
+		return 0
+	}
+	return fuel + computeRec(fuel)
 }
 
 func main() {
-	filePath := os.Args[1]	
-
-	file, err := os.Open(filePath)
+	file, err := os.Open(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
-
 	sum := 0.0
 	for scanner.Scan() {
 		mass, err := strconv.ParseFloat(scanner.Text(), 64)
 		if err != nil {
-                	log.Fatal(err)
-          	}
+			log.Fatal(err)
+		}
 		sum += computeRec(mass)
 	}
 
